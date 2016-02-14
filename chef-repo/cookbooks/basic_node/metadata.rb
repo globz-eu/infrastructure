6cfd07147bd8cd5285591e311e098809f24e0cdc
Stefan Dieterle <golgoths@yahoo.fr>
2016-02-15 00:31:09 +0100

    Incremented basic_node cookbook version. Adapted data_bags

--- chef-repo/cookbooks/basic_node/metadata.rb
+++ chef-repo/cookbooks/basic_node/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures basic_node'
 long_description 'Installs/Configures basic_node'
-version '0.1.7'
+version '0.1.8'
 
 depends 'apt', '~> 2.9.2'
 depends 'test-helper'
