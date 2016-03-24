b7e906379797ec0716aff9b1c1723413056b38e4
Stefan Dieterle <golgoths@yahoo.fr>
2016-03-24 11:03:58 +0100

    Added serverspec test for succesful converge of default recipe

--- chef-repo/cookbooks/basic_node/test/integration/default/serverspec/default_spec.rb
+++ chef-repo/cookbooks/basic_node/test/integration/default/serverspec/default_spec.rb
@@ -21,6 +21,16 @@ require 'spec_helper'
 
 set :backend, :exec
 
+describe file('/var/log/chef-kitchen/chef-client.log') do
+  it { should exist }
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  it { should be_mode 644 }
+  its(:content) { should_not match(/ERROR/)}
+  its(:content) { should_not match(/FATAL/)}
+end
+
 describe package('unattended-upgrades') do
   it { should be_installed }
 end
