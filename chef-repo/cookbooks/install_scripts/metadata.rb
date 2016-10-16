9aa6997fea09f2cbca9cda7805eb0e651202b783
Stefan Dieterle <golgoths@yahoo.fr>
2016-10-16 19:38:22 +0200

    Incremented install_scripts version.

--- chef-repo/cookbooks/install_scripts/metadata.rb
+++ chef-repo/cookbooks/install_scripts/metadata.rb
@@ -23,7 +23,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Creates user, Installs/Configures install_scripts'
 long_description 'Creates user, Installs/Configures install_scripts'
-version '0.1.11'
+version '0.1.12'
 
 depends 'apt', '~> 4.0.1'
 depends 'test-helper'
